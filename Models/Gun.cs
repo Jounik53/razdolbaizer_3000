9a390a4627f870107e4a0141777c26a10476865e
ShalomGustav <maximvaryagov@gmail.com>
2021-11-10 21:43:21 +0300

    Добавление полей, конфигурации , а так же логика критов (bool)

--- Models/Gun.cs
+++ Models/Gun.cs
@@ -8,6 +8,8 @@ namespace razdolbaizer_3000.Models
         public string Calibr { set; get; }
         public int Magazine { set; get; }
         public double Damage { get; set; }
+        public double Critdamage { get; set; }  //урон от крита
+        public double Chancecrit { get; set; } // шанс крита(процент)
         public bool Load 
         {
             get
@@ -20,6 +22,21 @@ namespace razdolbaizer_3000.Models
                 return true;
             }
         }
+
+        public bool Chance
+        {
+            get
+            {
+                if (Chancecrit >= 1)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+        }
+
+
         public int Misfire { get; set; }
 
         public void Initialization()
